3d27d2f8d0dddb52b7b780f2505ffa564e03a201
karabe <karabe000@gmail.com>
2014-03-11 13:59:41 +0900

    autocompleteで大文字小文字を区別するように

--- init.el
+++ init.el
@@ -256,6 +256,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ac-ignore-case nil)
  '(anzu-minimum-input-length 2)
  '(anzu-mode-lighter "")
  '(anzu-search-threshold 1000)
