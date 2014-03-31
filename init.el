0492538fef066d470453426d33a2773d0bb008a6
karabe <karabe000@gmail.com>
2014-03-31 11:47:25 +0900

    インデントをタブにする機能をweb-modeにも対応するようにした

--- init.el
+++ init.el
@@ -244,7 +244,10 @@
   "インデントをタブにする"
   (interactive)
   (setq indent-tabs-mode t)
-  (setq c-basic-offset 4))
+  (setq c-basic-offset 4)
+  (setq web-mode-markup-indent-offset 4)
+  (setq web-mode-css-indent-offset 4)
+  (setq web-mode-code-indent-offset 4))
 
 (global-set-key (kbd "C-.") 'other-window)
 (global-set-key (kbd "C-x C-p") 'find-file-at-point)
