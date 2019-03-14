307aa343ac67aa572d39a27608f0c7d802ed8e7f
karabe <karabe000@gmail.com>
2019-03-14 12:22:10 +0900

    enable company-mode in sql-mode

--- init.el
+++ init.el
@@ -10,7 +10,7 @@
         ;; ("org"          . "https://orgmode.org/elpa/")
         ;; ("melpa-stable" . "https://stable.melpa.org/packages/")
         ("melpa"        . "https://melpa.org/packages/")
-	))
+    ))
 (package-initialize)
 (unless package-archive-contents (package-refresh-contents))
 
@@ -65,7 +65,7 @@
   (php-mode-coding-style 'psr2))
 
 (use-package company
-  :hook ((php-mode web-mode css-mode js-mode emacs-lisp-mode) . company-mode)
+  :hook ((php-mode web-mode css-mode js-mode emacs-lisp-mode sql-mode) . company-mode)
   :custom
   (company-dabbrev-code-everywhere t)
   (company-dabbrev-downcase nil)
@@ -385,6 +385,78 @@
   (recentf-mode t))
 
 (use-package sql
+  :hook (sql-mode . (lambda ()
+                      (push `(sql-mode .
+                                       ,(apply 'company-keywords-upper-lower
+                                               (append
+                                                sql-mysql-functions
+                                                sql-mysql-keywords
+                                                sql-mysql-data-types)))
+                            company-keywords-alist)
+                      (setq-local company-backends '((company-keywords :with company-dabbrev-code)))))
+  :config
+  (defvar sql-mysql-functions
+    '("ascii" "avg" "bdmpolyfromtext" "bdmpolyfromwkb" "bdpolyfromtext"
+      "bdpolyfromwkb" "benchmark" "bin" "bit_and" "bit_length" "bit_or"
+      "bit_xor" "both" "cast" "char_length" "character_length" "coalesce"
+      "concat" "concat_ws" "connection_id" "conv" "convert" "count"
+      "curdate" "current_date" "current_time" "current_timestamp" "curtime"
+      "elt" "encrypt" "export_set" "field" "find_in_set" "found_rows" "from"
+      "geomcollfromtext" "geomcollfromwkb" "geometrycollectionfromtext"
+      "geometrycollectionfromwkb" "geometryfromtext" "geometryfromwkb"
+      "geomfromtext" "geomfromwkb" "get_lock" "group_concat" "hex" "ifnull"
+      "instr" "interval" "isnull" "last_insert_id" "lcase" "leading"
+      "length" "linefromtext" "linefromwkb" "linestringfromtext"
+      "linestringfromwkb" "load_file" "locate" "lower" "lpad" "ltrim"
+      "make_set" "master_pos_wait" "max" "mid" "min" "mlinefromtext"
+      "mlinefromwkb" "mpointfromtext" "mpointfromwkb" "mpolyfromtext"
+      "mpolyfromwkb" "multilinestringfromtext" "multilinestringfromwkb"
+      "multipointfromtext" "multipointfromwkb" "multipolygonfromtext"
+      "multipolygonfromwkb" "now" "nullif" "oct" "octet_length" "ord"
+      "pointfromtext" "pointfromwkb" "polyfromtext" "polyfromwkb"
+      "polygonfromtext" "polygonfromwkb" "position" "quote" "rand"
+      "release_lock" "repeat" "replace" "reverse" "rpad" "rtrim" "soundex"
+      "space" "std" "stddev" "substring" "substring_index" "sum" "sysdate"
+      "trailing" "trim" "ucase" "unix_timestamp" "upper" "user" "variance")
+    "MySQL Functions")
+  (defvar sql-mysql-keywords
+    '("action" "add" "after" "against" "all" "alter" "and" "as" "asc"
+      "auto_increment" "avg_row_length" "bdb" "between" "by" "cascade"
+      "case" "change" "character" "check" "checksum" "close" "collate"
+      "collation" "column" "columns" "comment" "committed" "concurrent"
+      "constraint" "create" "cross" "data" "database" "default"
+      "delay_key_write" "delayed" "delete" "desc" "directory" "disable"
+      "distinct" "distinctrow" "do" "drop" "dumpfile" "duplicate" "else" "elseif"
+      "enable" "enclosed" "end" "escaped" "exists" "fields" "first" "for"
+      "force" "foreign" "from" "full" "fulltext" "global" "group" "handler"
+      "having" "heap" "high_priority" "if" "ignore" "in" "index" "infile"
+      "inner" "insert" "insert_method" "into" "is" "isam" "isolation" "join"
+      "key" "keys" "last" "left" "level" "like" "limit" "lines" "load"
+      "local" "lock" "low_priority" "match" "max_rows" "merge" "min_rows"
+      "mode" "modify" "mrg_myisam" "myisam" "natural" "next" "no" "not"
+      "null" "offset" "oj" "on" "open" "optionally" "or" "order" "outer"
+      "outfile" "pack_keys" "partial" "password" "prev" "primary"
+      "procedure" "quick" "raid0" "raid_type" "read" "references" "rename"
+      "repeatable" "restrict" "right" "rollback" "rollup" "row_format"
+      "savepoint" "select" "separator" "serializable" "session" "set"
+      "share" "show" "sql_big_result" "sql_buffer_result" "sql_cache"
+      "sql_calc_found_rows" "sql_no_cache" "sql_small_result" "starting"
+      "straight_join" "striped" "table" "tables" "temporary" "terminated"
+      "then" "to" "transaction" "truncate" "type" "uncommitted" "union"
+      "unique" "unlock" "update" "use" "using" "values" "when" "where"
+      "with" "write" "xor")
+    "MySQL Keywords")
+  (defvar sql-mysql-data-types
+    '("bigint" "binary" "bit" "blob" "bool" "boolean" "char" "curve" "date"
+      "datetime" "dec" "decimal" "double" "enum" "fixed" "float" "geometry"
+      "geometrycollection" "int" "integer" "line" "linearring" "linestring"
+      "longblob" "longtext" "mediumblob" "mediumint" "mediumtext"
+      "multicurve" "multilinestring" "multipoint" "multipolygon"
+      "multisurface" "national" "numeric" "point" "polygon" "precision"
+      "real" "smallint" "surface" "text" "time" "timestamp" "tinyblob"
+      "tinyint" "tinytext" "unsigned" "varchar" "year" "year2" "year4"
+      "zerofill")
+    "MySQL Data Types")
   :custom
   (sql-product 'mysql))
 
