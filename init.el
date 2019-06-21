ae5dc15e791b2a89775d36a76db815c9e084f967
karabe <karabe000@gmail.com>
2019-06-21 13:31:40 +0900

    set (rustic-lsp-server 'rust-analyzer)

--- init.el
+++ init.el
@@ -90,7 +90,8 @@
               ("C-c C-b" . rustic-cargo-build)
               ("C-c C-l" . rustic-cargo-clippy))
   :custom
-  (rustic-flycheck-setup-mode-line-p nil))
+  (rustic-flycheck-setup-mode-line-p nil)
+  (rustic-lsp-server 'rust-analyzer))
 
 (use-package company
   :hook ((web-mode css-mode emacs-lisp-mode sql-mode lsp-mode graphviz-dot-mode)
