808d6419c1102ae6548605a5d2e869d19b8028c4
karabe <karabe000@gmail.com>
2020-01-14 15:12:36 +0900

    ediff-window-setup-function 'ediff-setup-windows-plain

--- init.el
+++ init.el
@@ -385,7 +385,8 @@
 
 (use-package ediff
   :custom
-  (ediff-split-window-function 'split-window-horizontally))
+  (ediff-split-window-function 'split-window-horizontally)
+  (ediff-window-setup-function 'ediff-setup-windows-plain))
 
 (use-package sql
   :hook (sql-mode . (lambda ()
